c0adf68ece97fc0a0686b4928c4f63589e181ffe
aganzhenko <hogvarce@gmail.com>
2018-07-07 13:59:40 +0300

    delete timer

--- src/components/TimerItem/TimerItem.js
+++ src/components/TimerItem/TimerItem.js
@@ -2,8 +2,10 @@ import React, { PureComponent }  from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames';
 import Timer from 'react-timer-component';
+import { Modal } from 'react-bootstrap';
 import Countdown from './components/Countdown';
 import iconSetting from 'static/icon_settings.svg';
+import alarm_delete from 'static/ic-timerdelete.png';
 import styles from './TimerItem.css';
 
 export default class TimerItem extends PureComponent {
@@ -16,12 +18,27 @@ export default class TimerItem extends PureComponent {
 
     state = {
         settingsIsOpen: false,
+        show: false,
     };
 
     onToggleSettings = () => {
         this.setState({
             settingsIsOpen: !this.state.settingsIsOpen,
         });
+    };
+
+    handleShow = () => {
+        this.setState({ show: true });
+    };
+
+    handleClose = () => {
+        this.setState({ show: false });
+    };
+
+    onDelete = () => {
+        const { id, deleteTimer } = this.props;
+        deleteTimer(id);
+        this.handleClose();
     };
 
     render() {
@@ -41,12 +58,24 @@ export default class TimerItem extends PureComponent {
                 <div className={styles.clear} />
                 <div className={settingsClass}>
                     <div className={styles.reset}>Reset Timer</div>
-                    <div className={styles.delete}>Delete Timer</div>
+                    <div className={styles.delete} onClick={this.handleShow}>Delete Timer</div>
                 </div>
                 <div className={styles.icon_setting} onClick={this.onToggleSettings}>
                     <img src={iconSetting} alt="settings" />
                 </div>
                 <div className={styles.stop}>Stop Timer</div>
+                <Modal show={this.state.show} onHide={this.handleClose}>
+                    <Modal.Body>
+                        <img className={styles.alarm_delete} src={alarm_delete} alt="delete timer" />
+                        <div className={styles.title_modal}>Delete Timer?</div>
+                        <div className={styles.clear_modal} />
+                        <div className={styles.submit}>
+                            <div className={styles.cancel} onClick={this.handleClose}>Cancel</div>
+                            <div className={styles.delete_timer} onClick={this.onDelete}>Delete</div>
+                        </div>
+                        <div className={styles.close} onClick={this.handleClose} />
+                    </Modal.Body>
+                </Modal>
             </div>
         );
     }
--- src/constants/timers.js
+++ src/constants/timers.js
@@ -3,6 +3,7 @@ import keyMirror from 'keymirror';
 const TimersConstants = keyMirror({
     GET_TIMERS_SUCCESS: null,
     ADD_NEW_TIMER_SUCCESS: null,
+    DELETE_TIMER_SUCCESS: null,
 });
 
 export default TimersConstants;
